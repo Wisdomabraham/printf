caeca4fc0bc9b702c71bb3d5f67d1d84c09cd81f
Wisdomabraham <wisdomabraham80@gmail.com>
2022-11-25 20:05:50 -0800

    printf project

--- print_address.c
+++ print_address.c
@@ -3,7 +3,7 @@
 /**
  * print_address - prints address of input in hexa format
  * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
+ * @f: pointer to the struct flags that determine
  * if a flag is passed to _printf
  * Return: number of char printed
  */
@@ -11,13 +11,11 @@ int print_address(va_list l, flags_t *f)
 {
 	char *str;
 	unsigned long int p = va_arg(l, unsigned long int);
-
 	register int count = 0;
 
 	(void)f;
-
 	if (!p)
-		return (_puts("(nil)"));
+	return (_puts("(nil)"));
 	str = convert(p, 16, 1);
 	count += _puts("0x");
 	count += _puts(str);
